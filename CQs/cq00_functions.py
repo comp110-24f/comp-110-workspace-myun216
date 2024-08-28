156f84b78dbbcfa410084ff5c557ecd988196e0a
Mayayun <Mayayun@unc.ad.edu>
2024-08-28 09:44:18 -0400

    Finished function "main"

--- CQs/cq00_functions.py
+++ CQs/cq00_functions.py
@@ -6,3 +6,12 @@ __author__ = "730462791"
 def mimic(message: str) -> str:
     """This function will take yout input and repeat it back to you."""
     return message
+
+
+def main() -> None:
+    """This will print the result of calling mimic."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
