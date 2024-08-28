49a919dc0cc0c9ccc0783893f1ed46283a297831
Mayayun <Mayayun@unc.ad.edu>
2024-08-28 09:20:26 -0400

    Finished cq00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,8 @@
+"""Challenge question 00: functions"""
+
+__author__ = "730462791"
+
+
+def mimic(message: str) -> str:
+    """This function will take yout input and repeat it back to you."""
+    return message
